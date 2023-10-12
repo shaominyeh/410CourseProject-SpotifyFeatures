ef1ec86ac1992fddf88b30bc165218c48ae2c0b6
Shao-Min <shaominyeh@gmail.com>
2023-10-11 21:20:31 -0700

    Finishing main recommend system

--- preprocess.py
+++ preprocess.py
@@ -5,8 +5,11 @@ import pandas as pd
 
 def extract_columns():
   if not os.path.isfile('formatted_songs.csv'):
+    if os.path.isfile('songs/songs.dat'):
+      os.remove('songs/songs.dat')
     songs = pd.read_csv('spotify_songs.csv')
     songs = songs.dropna()
+    songs = songs.drop_duplicates()
     songs = songs[songs['language'] == 'en']
     songs = songs.drop(['track_album_id', 'playlist_id', 'language'], axis = 1)
     songs.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
--- testrecommend.py
+++ testrecommend.py
@@ -10,24 +10,28 @@ def load_ranker(cfg_file):
     return metapy.index.OkapiBM25(k1=1.66,b=0.72,k3=2.2) 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} config.toml".format(sys.argv[0]))
-        sys.exit(1)
-
     preprocess.extract_columns()
     preprocess.make_corpus()
     songs = pd.read_csv('formatted_songs.csv')
 
-    cfg = sys.argv[1]
+    cfg = "config.toml"
     idx = metapy.index.make_inverted_index(cfg)
     ranker = load_ranker(cfg)
 
-    query_name = "Russian Roulette"
+    query_name = "i just wanna rock"
     query = metapy.index.Document()
     query.content(query_name.lower())
 
-    top_k = 10
+    top_k = len(songs)
     results = ranker.score(idx, query, top_k)
 
+    count = 0
+    topset = set()
     for result in results:
-        print(songs.iloc[result[0]]['track_name'])
+        if count >= 10:
+            break
+        songpair = (songs.iloc[result[0]]['track_name'], songs.iloc[result[0]]['track_artist'])
+        if songpair not in topset:
+            print(songpair)
+            topset.add(songpair)
+            count += 1
