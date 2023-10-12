2ba5a70425c8e9d764d0e4a7a621912a6c41cc1b
Shao-Min <shaominyeh@gmail.com>
2023-10-12 14:52:45 -0700

    Adding functionality for separated ranking

--- testrecommend.py
+++ testrecommend.py
@@ -1,37 +1,61 @@
 import csv
 import math
 import metapy
+import numpy as np
 import pandas as pd
 import sys
 
 import preprocess
 
+def preprocess_tasks():
+    preprocess.extract_columns()
+    preprocess.make_corpus()
+    return pd.read_csv('data/formatted_songs.csv')
+
 def load_ranker(cfg_file):
     return metapy.index.OkapiBM25(k1=1.66,b=0.72,k3=2.2) 
 
-if __name__ == '__main__':
-    preprocess.extract_columns()
-    preprocess.make_corpus()
-    songs = pd.read_csv('data/formatted_songs.csv')
-
-    cfg = "config/config.toml"
+def combined_ranking(songs, user_query, config):
+    cfg = config
     idx = metapy.index.make_inverted_index(cfg)
     ranker = load_ranker(cfg)
 
-    query_name = "love"
+    query_name = user_query
     query = metapy.index.Document()
     query.content(query_name.lower())
 
-    # top_k = len(songs)
     results = ranker.score(idx, query)
 
-    count = 0
-    topset = set()
+    songs_list = np.zeros(len(songs))
     for result in results:
-        if count >= 10:
-            break
-        songpair = (songs.iloc[result[0]]['track_name'], songs.iloc[result[0]]['track_artist'])
-        if songpair not in topset:
-            print(songpair)
-            topset.add(songpair)
-            count += 1
+        songs_list[result[0]] += result[1]
+    return songs_list
+
+# def separated_ranking(songs, user_query, title_weight, artist_weight, lyrics_weight):
+
+#     return 
+
+def print_results(songs_list, top_k):
+    if (len(songs_list) == 0):
+        print("No Songs Returned")
+    else:
+        count = 0
+        topset = set()
+        songs_list = np.argsort(songs_list)[::-1]
+        for song in songs_list:
+            if count >= top_k:
+                break
+            songpair = (songs.iloc[song]['track_name'], songs.iloc[song]['track_artist'])
+            if songpair not in topset:
+                print(songpair)
+                topset.add(songpair)
+                count += 1
+
+if __name__ == '__main__':
+    songs = preprocess_tasks()
+    query = "love"
+
+    songs_list = combined_ranking(songs, query, "config/config.toml")
+    # songs_list = separated_ranking(songs, query, 0.3, 0.3, 0.4)
+
+    print_results(songs_list, 10)
