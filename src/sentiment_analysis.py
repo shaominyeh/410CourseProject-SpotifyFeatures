012fff00185cd25073e6f7a4404fafc91ace47d4
Shao-Min <shaominyeh@gmail.com>
2023-11-21 11:04:07 -0800

    Adding regression types

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -2,7 +2,7 @@
 import metapy
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
@@ -19,8 +19,8 @@ def tokenize_query(songs):
     tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
     tok = metapy.analyzers.LowercaseFilter(tok)
     tok = metapy.analyzers.Porter2Filter(tok)
-    # tok = metapy.analyzers.ListFilter(tok, "../config/stopwords.txt", \
-                                    #   metapy.analyzers.ListFilter.Type.Reject)
+    tok = metapy.analyzers.ListFilter(tok, "../config/stopwords.txt", \
+                                      metapy.analyzers.ListFilter.Type.Reject)
     filtered_lyrics = []
     for text in songs['lyrics']: # Tokenizes each song's lyrics
         tok.set_content(text.strip())
@@ -28,7 +28,7 @@ def tokenize_query(songs):
         filtered_lyrics.append(' '.join(tokens))
     return filtered_lyrics
 
-def train_model(X, y, random_state):
+def train_model(X, y, random_state, model_choice):
     """Trains the model using TF-IDF and regression."""
     X_train, X_test, y_train, y_test = \
         train_test_split(X, y, test_size=0.2, random_state=random_state)
@@ -37,7 +37,7 @@ def train_model(X, y, random_state):
     X_train = tfidf_model.fit_transform(X_train)
     X_test = tfidf_model.transform(X_test)
 
-    model = LinearRegression() # Linear Regression fitting
+    model = model_choice # Linear Regression fitting
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -62,17 +62,26 @@ def compute_words(tfidf_model, query_vector):
     sorted_scores = sorted(word_dict.items(), key=lambda x:x[1], reverse = True)
     return sorted_scores[:10], feature_names # Sort based on scores and return list
 
-def result_items(chosen_index, unified_state, is_tokenized_query):
+def result_items(chosen_index, unified_state, is_tokenized_query, regression_type):
+    """Main function to find sentiment analysis of a song."""
     songs = preprocess_tasks()
     if is_tokenized_query:
-        X = tokenize_query(songs)
-    else:
-        X = songs['lyrics']
+        songs['lyrics'] = tokenize_query(songs)
+    X = songs['lyrics']
     y = songs['valence']
 
-    user_tfidf_model, user_model, calculated_mse = train_model(X, y, unified_state)
+    if regression_type == "ridge":
+        model = Ridge()
+    elif regression_type == "lasso":
+        model = Lasso()
+    else:
+        model = LinearRegression()
+
+    # Trains the model and outputs sentiment score
+    user_tfidf_model, user_model, calculated_mse = train_model(X, y, unified_state, model)
     model_predicted_score, song_actual_score, new_query_vector = compute_sentiment(\
         songs, chosen_index, user_model, user_tfidf_model)
+    # Computes the most significant words to sentiment score
     song_top_words, model_feature_names = compute_words(user_tfidf_model, new_query_vector)
     return model_predicted_score, song_actual_score,\
                  song_top_words, model_feature_names, calculated_mse
@@ -89,9 +98,10 @@ def print_items(predicted_score, actual_score, top_words, feature_names, mse):
 if __name__ == '__main__':
     CHOSEN_INDEX = 0 # Any int less than 15000
     UNIFIED_STATE = 47 # Any positive int
-    IS_TOKENIZED_QUERY = False # Bool
+    IS_TOKENIZED_QUERY = True # Bool
+    MODEL_NAME = "ridge" # "ridge", "lasso", "linear"
 
     main_predicted_score, main_actual_score, main_top_words, main_feature_names, main_mse\
-          = result_items(CHOSEN_INDEX, UNIFIED_STATE, IS_TOKENIZED_QUERY)
+          = result_items(CHOSEN_INDEX, UNIFIED_STATE, IS_TOKENIZED_QUERY, MODEL_NAME)
     print_items(main_predicted_score, main_actual_score,\
                  main_top_words, main_feature_names, main_mse)
